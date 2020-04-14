7de0e18528e2a2ade8ac18ac0f448fe32ffbd8e0
DIOGO-MOTA <diogomota21@gmail.com>
2020-04-14 20:41:56 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ messagem: 'cu' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started');
+    console.log('Server started');
 });
