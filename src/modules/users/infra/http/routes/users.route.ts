05840980a086f47bee78169a7574e4fd3159915f
DIOGO-MOTA <diogomota21@gmail.com>
2020-08-17 21:27:45 -0300

    INFRA

--- src/modules/users/infra/http/routes/users.route.ts
+++ src/modules/users/infra/http/routes/users.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
+import uploadConfig from '@config/upload';
 
-import CreateUserService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarSerce';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarSerce';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
