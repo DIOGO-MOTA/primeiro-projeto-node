1fe6e646d212cfd16be2718cb0dbafa939842309
DIOGO-MOTA <diogomota21@gmail.com>
2020-08-27 19:55:19 -0300

    DDD

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
 
 
 import Appointment from '../infra/typeorm/entities/Appointment';
@@ -12,9 +13,12 @@ interface IRequest {
 }
 
 
-
+@injectable()
 class CreateAppointmentService {
-    constructor (private appointmentsRepository: IAppointmentsRepository){}
+    constructor (
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository
+        ){}
 
     public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
 
