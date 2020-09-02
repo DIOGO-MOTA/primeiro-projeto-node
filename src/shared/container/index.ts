43e0ec9b68ac69bc406f6a406d6c7de9076ee23b
DIOGO-MOTA <diogomota21@gmail.com>
2020-09-01 22:10:28 -0300

    criando primeiro teste

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,4 @@
-import {container} from 'tsyringe';
+import { container } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -7,11 +7,11 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
-'AppointmentsRepository',
- AppointmentsRepository,
+    'AppointmentsRepository',
+    AppointmentsRepository,
 );
 
 container.registerSingleton<IUsersRepository>(
-'UsersRepository',
-UsersRepository,
+    'UsersRepository',
+    UsersRepository,
 );
