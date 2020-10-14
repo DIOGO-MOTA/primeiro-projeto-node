1724e60fb18e787b82a811835bede5494f31ede3
DIOGO-MOTA <diogomota21@gmail.com>
2020-10-14 16:26:26 -0300

    template de emails2

--- src/modules/users/infra/typeorm/entities/UserToken.ts
+++ src/modules/users/infra/typeorm/entities/UserToken.ts
@@ -10,11 +10,14 @@ import {
 @Entity('user_tokens')
 class UserToken {
     @PrimaryGeneratedColumn('uuid')
-    user_id: string;
+    id: string;
 
     @Column()
     @Generated('uuid')
     token: string;
+
+    @Column()
+    user_id: string;
 
     @CreateDateColumn()
     created_at: Date;
